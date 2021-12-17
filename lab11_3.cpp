310eb5fed49a3d14ebb41c484938b64818edf2aa
peakpunk <alk357951@gmail.com>
2021-12-17 18:10:19 +0700

    lab 11 complete

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,39 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+
+int main (){
+	ifstream source;
+	source.open("score.txt");
+
+    string textline;
+    double numline, sum, mean, sd;
+    int num = 0;
+    while (getline(source, textline))
+    {
+        numline = atof(textline.c_str());
+        sum += numline;
+        num++;
+    }
+    source.close();
+    mean = sum/num;
+
+    source.open("score.txt");
+    while (getline(source, textline))
+    {  
+        numline = atof(textline.c_str());
+        sd += pow((numline-mean),2);
+    }
+    source.close();
+    sd /= num;
+    sd = sqrt(sd);
+    cout << setprecision(3);
+    cout << "Number of data = " << num << endl;
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << sd << endl;
+	return 0;
+}
